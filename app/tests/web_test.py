e65bcd76f781ac4684eeaf3a330dc068f91fe442
Aelshafei <ahmad.elshaf3y@gmail.com>
2022-08-15 14:10:11 +0100

    initialize unit testing using pytest

---
+++ app/tests/web_test.py
@@ -0,0 +1,30 @@
+import pytest
+from prom_monitoring_urls_web_app import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+
+def test_index(client):
+    response = client.get('/')
+    assert '<h1>Prometheus metrics for URLs availability and response availiability</h1>' in str(response.data)
